c9f31d28bc1f845a67a37c70cf0d631aa4c08992
pgourav6464 <gouravpardeshi6464@gmail.com>
2024-02-06 02:29:33 -0500

    intro to js

---
+++ day_30_basic_js/script.js
@@ -0,0 +1,53 @@
+// console.log("hello ayz")
+
+// let number1 = 10
+// const number2 = 20
+
+
+// console.log(number1+number2)
+
+// let num = 30.2
+// console.log(num)
+
+// let Name = 'ayz'
+// console.log(Name)
+// let age18 =true 
+// console.log(age18)
+
+// let name1 = ' ayz '
+// let name2 = 'gourav'
+
+// console.log(
+//   ` "${name1}" , 'belong to bihar' , "${name2}"  , 'belong to indore' `
+// );
+
+// const num = 20
+
+// console.log(num)
+
+
+// ``````````````````````````````````````operater```````````````````````````````````````````
+
+
+// let num1 = 30
+// let num2 = 20
+
+// console.log(num1+num2)
+
+// console.log(num1*num2)
+// console.log(num1%num2)
+// console.log(num1/num2)
+// // 
+// console.log(num1++, --num1)
+
+// const number =20.5 
+
+// console.log(number ==20)
+// === type and value both checking 
+
+// console.log(typeof number);
+
+
+
+
+

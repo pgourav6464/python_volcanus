c4e1a6e5e5433d0c7a55173546e833b97aae2a43
pgourav6464 <gouravpardeshi6464@gmail.com>
2024-02-08 02:36:09 -0500

    js

---
+++ day31_jsbasiclast/script.js
@@ -0,0 +1,81 @@
+// const age = 18
+// const age = prompt("enter your age")
+
+// console.log("user age " ,age)
+
+// if(age>=18){
+//     const city = prompt("enter your city");
+//     if (city!='indore'){
+//         console.log("your ban from indore")
+//     }
+//     else{
+//         console.log("you can drive")
+//     }
+
+// }
+// else(
+//     console.log("you can't drive")
+// )
+
+// const dollar = 500 ;
+
+// if(dollar>=500){
+//     console.log("you can purchase i phone")
+// }
+
+// else{
+//     console.log("You need to sale your kidney... for buying i phone")
+// }
+
+//  () ? () : ()
+
+// const dollar = 500
+// (dollar < 500)
+//   ? console.log("you can purchase i phone")
+//   : console.log("You need to sale your kidney... for buying i phone"
+
+// const day = parseInt( prompt("enter day"))
+
+// switch(day){
+//     case 1 :
+//     console.log("monday")
+//     break ;
+//     case 2 :
+//     console.log("tuesday")
+//     break ;
+//     case 3 :
+//     console.log("wednesday")
+//     break ;
+//     case 4 :
+//     console.log("thursday")
+//     break ;
+//     case 5 :
+//     console.log("friday")
+//     break ;
+//     case 6 :
+//     console.log("saturday")
+//     break ;
+//     case 7 :
+//     console.log("holiday")
+//     break ;
+    
+//     default:
+//         console.log("invalid day")
+//         break;
+// }
+
+
+// normal function
+// function print(){
+//     console.log("print function create by me")
+// }
+
+// function with return value
+
+// function pi(){
+//     return "grp"
+// }
+
+// print();
+
+// console.log(pi())
---
+++ day32_function_objects_loops/script.js
@@ -0,0 +1,95 @@
+// // ~~~~~~~~~~~~~~~~~~~~~~~normal function ~~~~~~~~~~~~~~~~
+// function print(){
+//     console.log("This is function body")
+// }
+
+// print()
+
+// //~~~~~~~~~~~~~~~~~~ function with return type~~~~~~~~~~~~~~~~~
+
+// function sbiRateOfIntrest(){
+//     return 7.8
+// }
+
+// console.log(sbiRateOfIntrest())
+
+// ~~~~~~~~~~~~~~~~~~function with arguments~~~~~~~~~~~~~~~~~~
+
+// function sum (n1 , n2){
+//     return n1+n2
+// }
+
+// num=10
+// num1=20
+// console.log(sum(num,num1))
+
+// ~~~~~~~~~~~~~~~~ arrow function ~~~~~~~~~~~~~~~~~
+// function print(){
+//     console.log("this is normal function")
+// }
+
+// ~~~~~~~~~~~~~~~~ normal arrow function declare ~~~~~~~~~~~~~~~~~
+// const print2 = () => console.log("arrow function")
+
+// print2()
+
+// ~~~~~~~~~~~~~~~~ arrow function  with return value~~~~~~~~~~~~~~~~~
+
+// es6 me aya tha or one line function hai +code redability achi hoti h
+
+// const rateOfIntrest = () => 7.9
+
+// let result = rateOfIntrest()
+// console.log(result)
+
+// ~~~~~~~~~~~~~~~~ arrow function with arguments ~~~~~~~~~~~~~~~~~
+
+// const sum = (a,b)=> {
+//     return a+b
+// }
+// console.log(5,10)
+
+// ~~~~~~~~~~~~~~~ objects ~~~~~~~~~~~~~~~~~
+
+const lapy = {
+  modal_name: "hp probook 830 G8",
+  ram: "8Gb",
+  brand: "Hp",
+  ssd: "256gb",
+  processor :"intel i 12 ",
+  price :78520
+};
+console.log("lap1obj", lapy);
+
+
+const {ram , ssd}= lapy
+console.log(ram ,ssd)
+
+
+
+
+
+
+// ~~~~~~~~~~~~~~~~~spread operater~~~~~~~~~~~~~~~~~~~~~~~~
+
+const lapy1 ={...lapy , price:25000 , ssd:"512gb"}
+
+console.log("lap2obj",lapy1)
+
+
+// let accountno = 454879754;
+// const account_open = (name, pan, adhar, dob) => {
+//   console.log(
+//     `hello ${name} your account oepn successfull  your acc no is ${++accountno}\n `
+//   );
+// };
+
+// account_open("ashish ", "bgfr7878B", 5455998545, 85464);
+// account_open("grp ", "GGfr7878B", 54556563145, 85464);
+
+// const  = { 
+//   username: "ashish",
+//   pan_no: 84697889,
+//   adhar_no: 684618946,
+//   dob: 08 / 07 / 2001,
+// };
